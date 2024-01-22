6d29178080451f67a490bcc66c8a8446e57ff04e
Camille Rubio <rubio.camille1997@gmail.com>
2024-01-22 15:26:37 +0100

    hotfix autogenerate migration

--- alembic/env.py
+++ alembic/env.py
@@ -5,6 +5,8 @@ from sqlalchemy import pool
 
 from alembic import context
 
+from models import Base
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -18,10 +20,12 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+#target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
+
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
 
@@ -39,7 +43,6 @@ def run_migrations_offline() -> None:
 
     """
     url = config.get_main_option("sqlalchemy.url")
-    print(url)
     context.configure(
         url=url,
         target_metadata=target_metadata,
@@ -65,10 +68,7 @@ def run_migrations_online() -> None:
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
-    print(config.__dict__)
     with connectable.connect() as connection:
-        print(connection.__dict__)
-        print(target_metadata)
         context.configure(
             connection=connection, target_metadata=target_metadata
         )
